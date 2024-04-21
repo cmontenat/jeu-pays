cb6f2647e3285992089c1b2c8016bd0deb09540a
cmontenat <145672984+cmontenat@users.noreply.github.com>
2024-04-21 08:46:50 +0200

    Update code.js

--- jeu_pays/code.js
+++ jeu_pays/code.js
@@ -1,49 +1,38 @@
-// Jeu des pays
+// Tableau des pays
+        var tableauPays = [];
+        tableauPays[1] = 'Azerbaïdjan';
+        tableauPays[2] = 'Turkménistan';
+        tableauPays[3] = 'Ouzbékistan';
+        tableauPays[4] = 'Afghanistan';
+        tableauPays[5] = 'Pakistan';
+        tableauPays[6] = 'Tadjikistan';
+        tableauPays[7] = 'Kirghizistan';
 
-
-
-
-//Tableau des pays
-tableauPays = new Array()
-tableauPays[1] = 'Azerbaïdjan';
-tableauPays[2] = 'Turkménistan';
-tableauPays[3] = 'Ouzbékistan';
-tableauPays[4] = 'Afghanistan';
-tableauPays[5] = 'Pakistan';
-tableauPays[6] = 'Tadjikistan';
-tableauPays[7] = 'Kirghizistan';
-
-// fonction Valider
-function fonctionValider(){
-    //boucle sur les listes
-    for (var i=1; i<=7; i++)
-    {
-        // récupération de l'index, puis de la valeur choisie
-        var indexChoisi = document.forms["listes"].elements["liste"+i].selectedIndex;
-        var paysChoisi = document.forms["listes"].elements["liste"+i].options[indexChoisi].value;
-        // teste si la liste i affiche le bon pays
-        if(paysChoisi == tableauPays[i]){
-            //changement de style de l'élément numéro i
-            //document.getElementById("numero"+i).style.backgroundColor='#0066CC';
-            document.getElementById("numero"+i).className='OK';
-        } else {
-            //document.getElementById("numero"+i).style.backgroundColor='#FFFFFF';
-            document.getElementById("numero"+i).className='NOK';
+        // Fonction Valider
+        function fonctionValider() {
+            // Boucle sur les listes
+            for (var i = 1; i <= 7; i++) {
+                // Récupération de l'index et de la valeur choisie
+                var indexChoisi = document.forms["listes"].elements["liste" + i].selectedIndex;
+                var paysChoisi = document.forms["listes"].elements["liste" + i].options[indexChoisi].value;
+                // Teste si la liste i affiche le bon pays
+                if (paysChoisi == tableauPays[i]) {
+                    // Changement de classe pour l'élément numéro i (OK)
+                    document.getElementById("numero" + i).className = 'OK';
+                } else {
+                    // Changement de classe pour l'élément numéro i (NOK)
+                    document.getElementById("numero" + i).className = 'NOK';
+                }
+            }
         }
 
-
+        // Fonction Reset
+        function fonctionReset() {
+            // Boucle sur les listes
+            for (var i = 1; i <= 7; i++) {
+                // Remise à zéro des sélections
+                document.forms["listes"].elements["liste" + i].selectedIndex = 0;
+                // Suppression de la classe pour l'élément numéro i
+                document.getElementById("numero" + i).className = '';
+            }
         }
-
-    }
-//fonction Reset
-function fonctionReset(){
-    // boucle sur les listes
-    for (var i=1; i<=7; i++)
-    {
-        // affichage première valeur
-        document.forms["listes"].elements["liste"+i].selectedIndex = 0;
-        // document.getElementById("numero"+i).style.backgroundColor='#FFFFFF';
-        document.getElementById("numero"+i).className='';
-    }
-}
-
